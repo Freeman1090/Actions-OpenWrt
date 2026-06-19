28b16d6b4140195ebc6675fbe5cfd8381506e55c
Freeman1090 <dmitriyvsevolodovitch@gmail.com>
2026-06-19 16:56:57 +0300

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,26 +1,3 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-# Убираем kernel-драйверы QModem — они конфликтуют с ядром OpenWrt 25.12
-# Удаляем только конфликтующие патчи ядра
-# но оставляем сами пакеты драйверов
-rm -rf feeds/qmodem/driver/quectel_QMI_WWAN/patches
-rm -rf feeds/qmodem/driver/fibocom_QMI_WWAN/patches
-rm -rf feeds/qmodem/driver/simcom_QMI_WWAN/patches
+[ -f $GITHUB_WORKSPACE/files/usr/bin/modem-init.sh ] && chmod +x $GITHUB_WORKSPACE/files/usr/bin/modem-init.sh
+[ -f $GITHUB_WORKSPACE/files/etc/init.d/modem-init ] && chmod +x $GITHUB_WORKSPACE/files/etc/init.d/modem-init
